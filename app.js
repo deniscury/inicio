2e316f0ded112e305bdd0d39521019d5fc1578e3
denim95 <99043807+denim95@users.noreply.github.com>
2022-09-28 11:56:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var calculadora = require("./calculadora");
+
+console.log(calculadora.soma(10, 2));
+console.log(calculadora.sub(10, 2));
+console.log(calculadora.div(10, 2));
+console.log(calculadora.mult(10, 2));
+
+/*
+console.log('Hello World');
+var mostrarSite = true;
+const site = "www.google.com";
+
+if (mostrarSite){
+    console.log(site);
+}
+*/
